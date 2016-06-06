dde6e6d2edf90a8dcbfa0dc961a1be45f5f6842b
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-06-06 22:01:41 +0200

    local online function

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 /*jshint node:true, laxcomma:true*/
 /*eslint global-require:"warn"*/
 'use strict';
-var ccore = require('castor-core');
+var ccore = require('castor-core'),
+    kuler = require('kuler')
+;
 
 module.exports = function(config, start) {
   config.set('theme', __dirname);
   config.set('timeout', 1E6);
-  start();
+  start(function online(err, server) {
+    if (err instanceof Error) {
+      console.error(kuler("Unable to init the server.", "red"), kuler(err.toString(), 'orangered'));
+      process.exit(3);
+      return;
+    }
+    var pack = config.get('package');
+    if (pack) {
+      console.info(kuler('App detected.', 'olive'), kuler(pack.name + ' ' + pack.version, 'limegreen'));
+    }
+    console.info(kuler('Server is listening.', 'olive'),  kuler(config.get('baseURL') + "/", "limegreen"));
+  });
 };
 
 if (!module.parent) {
