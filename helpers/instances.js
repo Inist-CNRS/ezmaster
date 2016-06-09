94731d8d6f0dc8176006a3ab13b05ae7d5c25cb6
JulienCoyard <julien.coyard@gmail.com>
2016-06-09 14:00:56 +0200

    bithound fixes on instances.js

--- helpers/instances.js
+++ helpers/instances.js
@@ -1,86 +1,81 @@
+/*eslint no-sync: "off"*/
+
+'use strict';
+
 var path = require('path')
   , basename = path.basename(__filename, '.js')
   , debug = require('debug')('castor:route:' + basename)
-  , bodyParser = require('body-parser')
   , moment = require('moment')
-  , util = require('util')
+  //, util = require('util')
   , fs = require('fs')
-  , getSize = require('get-folder-size')
-  , filesize = require('filesize')
   , Docker = require('dockerode')
   , docker = new Docker({ socketPath: '/var/run/docker.sock'})
-  , exec = require('child_process').exec
-  , jsonfile = require('jsonfile')
-  , mkdirp = require('mkdirp')
-  , rimraf = require('rimraf')
-  , fileExists = require('file-exists')
-  , instances = require('../helpers/instances');
+  , jsonfile = require('jsonfile');
 
 
 
 module.exports.getInstances = function (req, res, next) {
 
-    var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
-    docker.listContainers({all : true}, function (err, containers) {
+  var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
+  docker.listContainers({all : true}, function (err, containers) {
 
-      if (err) { return next(err); }
-      
-      var arrayObject = [];
+    if (err) { return next(err); }
 
-      (function check () {
+    var arrayObject = [];
 
-          var elements = containers.pop()
-            , container = {};
+    (function check () {
 
-          if (!elements) { return res.status(200).send(arrayObject); }
+      var elements = containers.pop()
+      , container = {};
 
-          var splittedName = elements.Names[0].split('/');
+      if (!elements) { return res.status(200).send(arrayObject); }
 
-          if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
+      var splittedName = elements.Names[0].split('/');
 
-          var img = docker.getImage(elements.Image);
+      if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
 
-          jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
-          , function (err, obj) {
+      var img = docker.getImage(elements.Image);
 
-            if (err) { return next(err); }
+      jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
+      , function (err, obj) {
 
-            img.inspect(function (err, data) {
+        if (err) { return next(err); }
 
-              if (err) { return next(err); }
+        img.inspect(function (err, data) {
 
-              if (elements.State === 'running') {
-                container['status'] = true;
+          if (err) { return next(err); }
 
-                container['address'] = 'http://'+process.env.EZMASTER_PUBLIC_IP+':'+elements.Ports[0].PublicPort;
-                if(!process.env.EZMASTER_PUBLIC_IP)
-                  container['address'] = 'http://127.0.0.1:'+elements.Ports[0].PublicPort;
+          if (elements.State === 'running') {
+            container['status'] = true;
 
-                container['target'] = splittedName[1];
-              }
-              else if (elements.State === 'exited') {
-                container['status'] = false;
-                container['address'] = '';
-                container['target'] = '';
-              }
+            container['address'] = 'http://'+process.env.EZMASTER_PUBLIC_IP
+            +':'+elements.Ports[0].PublicPort;
 
-              elements.Image = data.RepoTags[0];
-              elements.Names[0] = splittedName[1];
-              elements.Created = moment.unix(elements.Created).format('YYYY/MM/DD');
+            if (!process.env.EZMASTER_PUBLIC_IP)
+              container['address'] = 'http://127.0.0.1:'+elements.Ports[0].PublicPort;
 
-              container['longName'] = obj.longName;
-              container['description'] = elements;
+            container['target'] = splittedName[1];
+          }
+          else if (elements.State === 'exited') {
+            container['status'] = false;
+            container['address'] = '';
+            container['target'] = '';
+          }
 
-              arrayObject.push(container);
-              check();
+          elements.Image = data.RepoTags[0];
+          elements.Names[0] = splittedName[1];
+          elements.Created = moment.unix(elements.Created).format('YYYY/MM/DD');
 
-            });
-            
-          });
+          container['longName'] = obj.longName;
+          container['description'] = elements;
 
-      })();
-    
-    });
+          arrayObject.push(container);
+          check();
+
+        });            
+      });
+    })();
+  });
 
 };
 
@@ -88,51 +83,46 @@ module.exports.getInstances = function (req, res, next) {
 
 module.exports.getInstancesReverseProxy = function () {
 
-    var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
-    docker.listContainers({all : true}, function (err, containers) {
-      
-      var arrayObject = [];
+  var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
 
-      (function check () {
+  docker.listContainers({all : true}, function (err, containers) {
+        
+    var arrayObject = [];
 
-          var elements = containers.pop();
-          var container = {};
+    (function check () {
 
-          if (!elements) { return arrayObject; }
+      var elements = containers.pop();
+      var container = {};
 
-          var splittedName = elements.Names[0].split('/');
+      if (!elements) { return arrayObject; }
 
-          if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
+      var splittedName = elements.Names[0].split('/');
 
-          var img = docker.getImage(elements.Image);
+      if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
 
-          jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
-          , function (err, obj) {
+      var img = docker.getImage(elements.Image);
 
-            img.inspect(function (err, data) {
+      jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
+      , function (err, obj) {
 
-              if (elements.State === 'running') {
-                // Only get id and port
-                container['id'] = elements.Id;
-                container['port'] = elements.Ports[0].PublicPort;
+        img.inspect(function (err, data) {
 
-              }
-              else if (elements.State === 'exited') {
-                // Only get id and port
-                container['id'] = elements.Id;
-                container['port'] = elements.Ports[0];
-                
-              }
+          if (elements.State === 'running') {
+            // Only get id and port
+            container['id'] = elements.Id;
+            container['port'] = elements.Ports[0].PublicPort;
+          }
+          else if (elements.State === 'exited') {
+            // Only get id and port
+            container['id'] = elements.Id;
+            container['port'] = elements.Ports[0];  
+          }
 
-              arrayObject.push(container);
-              check();
+          arrayObject.push(container);
+          check();
 
-            });
-            
-          });
-
-      })();
-    
-    });
-
+        });            
+      });
+    })();
+  });
 };
