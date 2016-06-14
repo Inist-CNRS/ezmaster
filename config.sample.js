7c8a60c810fb2bcc3a02dce3a7de6b06f969c860
Stéphane Gully <stephane.gully@gmail.com>
2016-06-14 22:34:04 +0200

    cleanup

--- config.sample.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = {
-  // here is your specific castor-boileplate application
-  // configurations parameters
-  title : 'The famous castor-boileplate' // example...
-};
