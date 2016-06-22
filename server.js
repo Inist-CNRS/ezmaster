b3cdcbeafca4558caee8d5ddf267a7adc77d94f4
githosting <alexandrebettinger@hotmail.fr>
2016-06-22 08:52:17 +0200

    Adding port 35267 in test.js

--- server.js
+++ server.js
@@ -9,12 +9,12 @@
 'use strict';
 
 // To have docker messages with beautiful colors on console.
-var kuler = require('kuler'); 
+var kuler = require('kuler');
 
 // config : variable contenant tous les éléments de la configuration de l'application.
 // start : fonction de callback définie un peu plus bas.
 module.exports = function(config, start) {
-  
+
   // Initialisation de la configuration de l'application avant le lancement du serveur web.
   config.set('theme', __dirname);
   config.set('timeout', 1E6);
@@ -24,20 +24,8 @@ module.exports = function(config, start) {
   // Récupération du serveur web dans la variable server si ça s'est bien passé.
   start(function online(err, server) {
 
-    // On utilise socket.io sur le serveur.
+    // Use socket.io on the server.
     var io = require('socket.io').listen(server);
-    
-    // TEST : Quand un client se connecte.
-    io.sockets.on('connection', function (socket) {
-      config.set('socket', socket);
-      
-      /*
-      // On envoie un message de type message_welcome au client : /views/template.html
-      var message = "Welcome";
-      socket.emit('message', message);
-      */
-
-    });
 
     if (err instanceof Error) {
       console.error(kuler("Unable to init the server.", "red"), kuler(err.toString(), 'orangered'));
@@ -49,6 +37,6 @@ module.exports = function(config, start) {
       console.info(kuler('App detected.', 'olive'), kuler(pack.name + ' ' + pack.version, 'limegreen'));
     }
     console.info(kuler('Server is listening.', 'olive'),  kuler(config.get('baseURL') + "/", "limegreen"));
-  
+
   });
 };
