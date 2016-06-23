e4c2755a8d833fdba292cdb0e4d1ee529061ea90
JulienCoyard <julien.coyard@gmail.com>
2016-06-23 10:07:01 +0200

    end of getInstances.js

--- helpers/instances.js
+++ helpers/instances.js
@@ -6,140 +6,138 @@ var path = require('path')
   , basename = path.basename(__filename, '.js')
   , debug = require('debug')('castor:route:' + basename)
   , moment = require('moment')
-  //, util = require('util')
+  , util = require('utile')
   , fs = require('fs')
+  , glob = require("glob")
   , Docker = require('dockerode')
   , docker = new Docker({ socketPath: '/var/run/docker.sock'})
   , jsonfile = require('jsonfile');
 
 
 
-module.exports.getInstances = function (req, res, next) {
+  module.exports.getInstances = function (cb) {
 
-  var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
-  docker.listContainers({all : true}, function (err, containers) {
 
-    if (err) { return next(err); }
+    util.async.parallel([
 
-    var arrayObject = [];
+      // get manifest de toutes les instances
+      function(handleManifests){  
+        var arrayTechnicalNames =[];
 
-    (function check () {
-
-      var elements = containers.pop()
-      , container = {};
-
-      if (!elements) { return res.status(200).send(arrayObject); }
-
-      var splittedName = elements.Names[0].split('/');
-
-      if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
-
-      var img = docker.getImage(elements.Image);
-
-      jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
-      , function (err, obj) {
-
-        if (err) { return next(err); }
-
-        img.inspect(function (err, data) {
-
-          if (err) { return next(err); }
-
-          if (elements.State === 'running') {
-            container['status'] = true;
-
-            container['address'] = 'http://'+process.env.EZMASTER_PUBLIC_IP
-            +':'+elements.Ports[0].PublicPort;
-
-            if (!process.env.EZMASTER_PUBLIC_IP)
-              container['address'] = 'http://127.0.0.1:'+elements.Ports[0].PublicPort;
-
-            container['target'] = splittedName[1];
+        glob("manifests/*.json", function(err, files) { // read the folder or folders if you want: example json/**/*.json
+          
+          if(err) {
+            console.log("cannot read the folder, something goes wrong with glob", err);
           }
-          else if (elements.State === 'exited') {
-            container['status'] = false;
-            container['address'] = '';
-            container['target'] = '';
-          }
+          var i = 1;
 
-          elements.Image = data.RepoTags[0];
-          elements.Names[0] = splittedName[1];
-          elements.Created = moment.unix(elements.Created).format('YYYY/MM/DD');
+          files.forEach(function(file) {
 
-          container['longName'] = obj.longName;
-          container['description'] = elements;
 
-          arrayObject.push(container);
-          check();
+            var technicalName = file.split('/')[1].split('.')[0];
+
+            fs.readFile(file, 'utf8', function (err, data) { // Read each file
+
+              if(err) {
+                console.log("cannot read the file, something goes wrong with the file", err);
+              }
+
+              var manifest = [];
+              var data = JSON.parse(data);
+              manifest['longName'] = data.longName;
+              manifest['technicalName'] = technicalName;
+              arrayTechnicalNames.push(manifest);
+              ;
+              
+
+              if(i===files.length){
+                //console.log("NAMES:"+util.inspect(arrayTechnicalNames));
+                handleManifests(null,arrayTechnicalNames);
+              }
+              i++;
+            });
+
+          });
+          
 
         });
-      });
-    })();
+        
+        
+      }, 
+
+      // docker.listContainers
+      function(handleContainers){
+
+        var arrayInstances=[];
+
+        docker.listContainers({all : true}, function (err, containers) {
+
+          containers.forEach(function(data){
+
+
+            var instance = [];
+
+
+            instance['technicalName'] = data.Names[0].split('/')[1];
+            instance['containerId'] = data.Id;
+            instance['dataPath'] = "datapath";
+            instance['creationDate'] = moment.unix(data.Created).format('YYYY/MM/DD')
+            instance['app'] = data.Image;     
+
+            if (data.State === 'running') {
+              instance['running'] = true;
+              instance['port'] = data.Ports[0].PublicPort;
+            }
+            else if (data.State === 'exited') {
+              instance['running'] = false;
+              instance['port'] = [];
+            }
+
+            arrayInstances.push(instance);
+
+
+
+          })
+          handleContainers(null,arrayInstances);
+         });
+
+        
+        
+        
+        
+        } 
+
+
+  ], function (err, results) {
+    // optional callback
+    // if pas dans le manifest alors on zap
+
+    // ignore containers having a unknow technical name
+    // (ezmaster him self or mongodb or other containers running on the machine)
+    //if (arrayTechnicalNames.indexOf(arrayInstances.technicalName) === -1) { return check(); }
+    var res=[];
+
+    //console.log(util.inspect(results));
+
+    results[0].forEach (function(data){
+
+      var manifest = data;
+
+      results[1].forEach(function(data){
+
+        var instance = data;
+
+        if (manifest.technicalName === data.technicalName) {
+
+              instance['longName']=manifest.longName;
+              res.push(instance);
+              
+                        
+        };
+      });   
+    });
+    return cb(null,res);
   });
+}
 
-};
-
-
-module.exports.getInstancesReverseProxy = function (callback) {
-
-  var instancesArray = fs.readdirSync(path.join(__dirname, '../instances/'));
-  
-
-  docker.listContainers({all : true}, function (err, containers) {
-
-    if (err) { return callback(err); }
-
-    var arrayObject = [];
-
-
-
-    (function check () {
-
-      var elements = containers.pop();
-      var container = {};
-
-      if (!elements) {          
-
-        return callback(null, arrayObject); 
-      }
-
-      var splittedName = elements.Names[0].split('/');
-
-      if (instancesArray.indexOf(splittedName[1]) === -1) { return check(); }
-
-      var img = docker.getImage(elements.Image);
-
-      jsonfile.readFile(path.join(__dirname, '../manifests/', splittedName[1] + '.json')
-      , function (err, obj) {
-
-        if (err) { return callback(err); }
-
-        img.inspect(function (err, data) {
-
-          if (err) { return callback(err); }
-
-          if (elements.State === 'running') {
-            // Only get id and port
-            container['id'] = elements.Id;
-            container['port'] = elements.Ports[0].PublicPort;
-          }
-          else if (elements.State === 'exited') {
-            // Only get id and port
-            container['id'] = elements.Id;
-            container['port'] = elements.Ports[0];
-          }
-
-          arrayObject.push(container);
-          check();
-
-        });
-      });
-    })();
-  });
-};
-
-
-
-
-
-
+ 
