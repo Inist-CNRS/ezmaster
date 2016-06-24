2ca8fee7686c7955734bbceb9d06f094f2710362
JulienCoyard <julien.coyard@gmail.com>
2016-06-24 18:15:00 +0200

    refactoring of getInstances :art:

--- helpers/instances.js
+++ helpers/instances.js
@@ -11,60 +11,56 @@ var path = require('path')
   , glob = require("glob")
   , Docker = require('dockerode')
   , docker = new Docker({ socketPath: '/var/run/docker.sock'})
-  , jsonfile = require('jsonfile');
+  , jsonfile = require('jsonfile')
+  , _ = require('lodash');
 
 
 
   module.exports.getInstances = function (cb) {
 
-
     util.async.parallel([
 
-      // get manifest de toutes les instances
-      function(handleManifests){
-        var arrayTechnicalNames = [];
+      // retrive all instances manifests from the files
+      function (handleManifests) {
 
+        // read the content of manifests folders in order to
+        // extract the instances technicalName and some metadata
+        // TODO: ajouter __dirname + "/../
+        glob('manifests/*.json', function (err, files) {
 
-
-        glob("manifests/*.json", function(err, files) { // read the folder or folders if you want: example json/**/*.json
-
-
-
-          if(err) {
-            console.log("cannot read the folder, something goes wrong with glob", err);
+          if (err) {
+            debug('cannot read the folder, something goes wrong with glob', err);
+            return handleManifests(err);
           }
 
-          var i = 1;
+          // if no files then return an empty array
+          if (files.length === 0) {            
+            return handleManifests(null, []);
+          }
 
-          if(files.length === 0)
-            handleManifests(null,arrayTechnicalNames);
+          var manifests = [];
+          files.forEach(function (file) {
 
-          files.forEach(function(file) {
-
-
-
+            // Extract the technicalName from the filename and read 
+            // the manifest content to get other metadata
+            // Notice: filename example 'manifests/myprj-mystudy-5.json'
+            //         then technicalName will be 'myprj-mystudy-5'
             var technicalName = file.split('/')[1].split('.')[0];
-
-            fs.readFile(file, 'utf8', function (err, data) { // Read each file
-
-              if(err) {
-                console.log("cannot read the file, something goes wrong with the file", err);
+            fs.readFile(file, 'utf8', function (err, manifestContent) {
+              if (err) {
+                debug('cannot read the file, something goes wrong with the file', err);
+                return handleManifests(err);
               }
 
-              var manifest = {};
-              var data = JSON.parse(data);
-              manifest['longName'] = data.longName;
-              manifest['technicalName'] = technicalName;
-              arrayTechnicalNames.push(manifest);
+              var manifest = JSON.parse(manifestContent);
+              manifest.technicalName = technicalName;
+              manifests.push(manifest);
 
-              if(i===files.length){
-
-                //console.log("NAMES:"+util.inspect(arrayTechnicalNames));
-                handleManifests(null,arrayTechnicalNames);
+              // return the manifests when the
+              // last file is handled
+              if (manifests.length == files.length) {
+                return handleManifests(null, manifests);
               }
-
-              i++;
-
             });
 
           });
@@ -75,87 +71,79 @@ var path = require('path')
 
       },
 
-      // docker.listContainers
-      function(handleContainers){
+      // Retrives the instances docker informations. Ex:
+      // - technicalName: 'article-type-4',
+      // - containerId: '1e254654654465446545465465456465465',
+      // (- dataPath: '/applis/lodex/home/instances/article-type-4')
+      // - creationDate: '2016/03/17',
+      // - port: 35284,
+      // - app: 'inistcnrs/lodex:2.0.1',
+      // - running: true
+      function (handleDockerInstances) {
 
-        var arrayInstances = [];
+        docker.listContainers({ all : true }, function (err, containers) {
 
-        docker.listContainers({all : true}, function (err, containers) {
-
-          containers.forEach(function(data){
+          var dockerInstances = [];
+          containers.forEach(function (data){
 
             var instance = {};
 
-            instance['technicalName'] = data.Names[0].split('/')[1];
-            instance['containerId'] = data.Id;
-            instance['dataPath'] = "datapath";
-            instance['creationDate'] = moment.unix(data.Created).format('YYYY/MM/DD')
-            instance['app'] = data.Image;
+            // Example of data.Names[0]: /myprj-mystudy-5
+            instance.technicalName = data.Names[0].split('/')[1];
+            instance.containerId   = data.Id;
+            instance.dataPath      = "datapath"; // TODO
+            instance.creationDate  = moment.unix(data.Created).format('YYYY/MM/DD')
+            instance.app           = data.Image;
 
             if (data.State === 'running') {
-              instance['running'] = true;
-              instance['port'] = data.Ports[0].PublicPort;
+              
+              instance.running = true;
+              instance.port    = data.Ports[0].PublicPort;
+              
+              // TODO: comments needed
+              //       maybe "address" is wrongly named ? publicUrl would be better ?
+              instance.address = 'http://' 
+                + process.env.EZMASTER_PUBLIC_IP 
+                + ':' + data.Ports[0].PublicPort;
+              if(!process.env.EZMASTER_PUBLIC_IP) {
+                instance.address = 'http://127.0.0.1:' + data.Ports[0].PublicPort;
+              }
+              instance.target = data.Names[0].split('/')[1];
 
-              instance['address'] = 'http://'+process.env.EZMASTER_PUBLIC_IP+':'+data.Ports[0].PublicPort;
-              if(!process.env.EZMASTER_PUBLIC_IP)
-                instance['address'] = 'http://127.0.0.1:'+data.Ports[0].PublicPort;
+            } else if (data.State === 'exited') {
 
-                instance['target'] = data.Names[0].split('/')[1];
+              instance.running = false;
+              instance.port    = [];
+              instance.address = '';
+              instance.target  = '';
 
             }
-            else if (data.State === 'exited') {
-              instance['running'] = false;
-              instance['port'] = [];
-              instance['address'] = '';
-              instance['target'] = '';
-            }
 
-            arrayInstances.push(instance);
+            dockerInstances.push(instance);
+          });
 
-          })
-          handleContainers(null,arrayInstances);
+          // once docker containers are parsed we return the 
+          // ezmaster formated instances list
+          return handleDockerInstances(null, dockerInstances);
         });
-
-
-
-
 
       }
 
 
   ], function (err, results) {
-    // optional callback
-    // if pas dans le manifest alors on zap
 
-    // ignore containers having a unknow technical name
-    // (ezmaster him self or mongodb or other containers running on the machine)
-    //if (arrayTechnicalNames.indexOf(arrayInstances.technicalName) === -1) { return check(); }
-
-    //var res=[];
-    var res = {}; // Le tableau associatif des instances que l'on retournera à la fin.
-
-    //console.log(util.inspect(results));
-
-    results[0].forEach (function(data){
-
-      var manifest = data;
-
-      results[1].forEach(function(data){
-
-        var instance = data;
-
-        if (manifest.technicalName === data.technicalName) {
-
-              instance['longName'] = manifest.longName;
-              //res.push(instance);
-              res[instance['technicalName']] = instance;
-
-              console.log("########## TECH NAME : " + instance['technicalName'] + " ##########");
-
-
+    // retrives results from the two callbacks (manifests files and docker metadata)
+    // and ignore docker containers not listed in the manifests ("unknown technicalName")
+    // (example: ezmaster itself or ezmaster_db or any other containers currently running on the machine)
+    var ezmasterInstances = {};
+    results[0].forEach (function (manifest) {
+      results[1].forEach(function (dockerInstance) {
+        if (manifest.technicalName === dockerInstance.technicalName) {          
+          ezmasterInstances[manifest.technicalName] = _.assign(dockerInstance, manifest);
         };
       });
     });
-    return cb(null,res);
+
+    return cb(null, ezmasterInstances);
   });
 }
