19c4f04cd2ad24a9724e897eaad278e513370168
Stéphane Gully <stephane.gully@gmail.com>
2016-06-27 23:29:32 +0200

    refactoring: few comments and indentation and renaming

--- middlewares/reverseproxy.js
+++ middlewares/reverseproxy.js
@@ -1,65 +1,85 @@
 /*jshint node:true, laxcomma:true */
 'use strict';
 
+/**
+ * 
+ * Example with curl to test the reverse proxy:
+ * export EZMASTER_PUBLIC_DOMAIN="data.istex.fr"
+ * make run-debug
+ * curl -v --proxy "" -H "X-Forwarded-Host: aa-bb-1.data.istex.fr" -H "X-Forwarded-Server: data.istex.fr" http://127.0.0.1:35267/
+ * curl -v --proxy "" -H "Host: aa-bb-1.data.istex.fr" http://127.0.0.1:35267/
+ * 
+ */
+
+var path      = require('path');
+var basename  = path.basename(__filename, '.js');
+var debug     = require('debug')('ezmaster:' + basename);
+
 var httpProxy = require('http-proxy');
 var instances = require('../helpers/instances');
-var path = require('path');
-var basename = path.basename(__filename, '.js');
-var debug = require('debug')('ezmaster:' + basename);
 
 
-module.exports = function(options, core, data) {
-  debug('Loading reverseproxy middleware');
+module.exports = function(options, core) {
 
   var proxy = httpProxy.createProxyServer({})
     , domainEnv = core.config.get('publicDomain');
 
+  debug('Loading reverseproxy middleware: ' + (domainEnv ? 'enabled [' + domainEnv + ']' : 'disabled'));
 
   return function(req, res, next) {
 
-    instances.getInstances(function (err, data) {
-      var reqServer = req.headers['x-forwarded-server']
-      , reqHost = req.headers['x-forwarded-host']
-      , reqSubdomain = reqHost ? reqHost.split('.') : null
+    instances.getInstances(function (err, instances) {
+      var host         = req.headers['host']
+        , reqServer    = req.headers['x-forwarded-server']
+        , reqHost      = req.headers['x-forwarded-host']
+        , reqSubdomain = reqHost ? reqHost.split('.') : false
       ;
 
-      debug('reverseproxy#1',reqSubdomain,' && (', reqServer, ' === ', domainEnv, ")");
+      debug('reverseproxy#1', host, ' ', reqSubdomain,' && (', reqServer, ' === ', domainEnv, ")");
 
-      if (reqSubdomain && (reqServer === domainEnv) && data !== undefined) {
+      // Two way to activate the RP:
+      // with an explicit "Host" header
+      // with the special X-Forwarded-* headers
+      var isRpEnabled = {}; 
+      isRpEnabled.byHost       = host.slice(-domainEnv.length) === domainEnv;
+      isRpEnabled.byXForwarded = reqSubdomain && (reqServer === domainEnv);
+      debug(isRpEnabled);
+
+      if (reqSubdomain && (reqServer === domainEnv) && instances !== undefined) {
 
 
         debug('reverseproxy#1.1');
 
         var search = reqSubdomain[0].split('-');
 
-        var found = Object.keys(data)
-        .map(function(z) {
-          data[z].current = data[z].technicalName.split('-');
-          data[z].current[2] = data[z].current[2] === undefined ? 0 : Number(data[z].current[2]);
-          if (Number.isNaN(data[z].current[2])) {
-            data[z].current[2] = 0;
-          }
-          return z;
-        })
-        .sort(function(a, b) {
-          return data[a].current[2] < data[b].current[2];
-        })
-        .filter(function(x) {
-          return data[x].current[0] === search[0] && data[x].current[1] === search[1];
-        })
-        .reduce(function(prev, w) {
-          if (prev !== undefined) {
-            return prev;
-          }
-          if (search[2] === undefined) {
-            return w;
-          }
-          if (data[w].current[0] === search[0] && data[w].current[1] === search[1]
-            && data[w].current[2] === Number(search[2])) {
-            return w;
-          }
-          return;
-        }, undefined);
+        var found = Object.keys(instances)
+          .map(function(z) {
+            instances[z].current = instances[z].technicalName.split('-');
+            instances[z].current[2] = instances[z].current[2] === undefined ? 0 : Number(instances[z].current[2]);
+            if (Number.isNaN(instances[z].current[2])) {
+              instances[z].current[2] = 0;
+            }
+            return z;
+          })
+          .sort(function (a, b) {
+            return instances[a].current[2] < instances[b].current[2];
+          })
+          .filter(function (x) {
+            return instances[x].current[0] === search[0] && instances[x].current[1] === search[1];
+          })
+          .reduce(function (prev, w) {
+            if (prev !== undefined) {
+              return prev;
+            }
+            if (search[2] === undefined) {
+              return w;
+            }
+            if (instances[w].current[0] === search[0] && instances[w].current[1] === search[1]
+              && instances[w].current[2] === Number(search[2])) {
+              return w;
+            }
+            return;
+          }, undefined);
 
         if (found !== undefined) {
           var url = 'http://'+found+':'+ '3000';
