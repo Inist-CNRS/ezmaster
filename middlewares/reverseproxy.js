1286534784e425fe5cbd2862dd801c0b4ebcb299
JulienCoyard <julien.coyard@gmail.com>
2016-06-27 13:55:35 +0200

    fix bithound

--- middlewares/reverseproxy.js
+++ middlewares/reverseproxy.js
@@ -1,10 +1,8 @@
 /*jshint node:true, laxcomma:true */
-"use strict";
+'use strict';
 
 var httpProxy = require('http-proxy');
 var instances = require('../helpers/instances');
-var util = require('utile');
-
 var path = require('path');
 var basename = path.basename(__filename, '.js');
 var debug = require('debug')('ezmaster:' + basename);
@@ -14,8 +12,7 @@ module.exports = function(options, core, data) {
   debug('Loading reverseproxy middleware');
 
   var proxy = httpProxy.createProxyServer({})
-    , domainEnv = core.config.get('publicDomain')
-    ,publicIP = core.config.get('publicIP');;
+    , domainEnv = core.config.get('publicDomain');
 
 
   return function(req, res, next) {
@@ -23,83 +20,66 @@ module.exports = function(options, core, data) {
 
 
 
-    instances.getInstances(function(err,data){
+    instances.getInstances(function (err, data) {
       var reqServer = req.headers['x-forwarded-server']
       , reqHost = req.headers['x-forwarded-host']
       , reqSubdomain = reqHost ? reqHost.split('.') : null
       ;
 
-
-    debug('reverseproxy#1',reqSubdomain,' && (', reqServer, ' === ', domainEnv, ")");
-
-    debug("");
-    debug("########## DEBUG ##########");
-    debug("DATA : " + data);
-    debug("REQHOST : " + reqHost);
-    debug("REQSUBDOMAIN : " + reqSubdomain);
-    debug("REQSERVER : " + reqServer);
-    debug("DOMAINENV : " + domainEnv);
-    debug("########## FIN DEBUG ##########");
-    debug("");
+      if (reqSubdomain && (reqServer === domainEnv) && data !== undefined) {
 
 
-    if(reqSubdomain && (reqServer === domainEnv) && data !== undefined) {
+        debug('reverseproxy#1.1');
 
+        var search = reqSubdomain[0].split('-');
 
-      debug('reverseproxy#1.1');
+        var found = Object.keys(data)
+        .map(function(z) {
+          data[z].current = data[z].technicalName.split('-');
+          data[z].current[2] = data[z].current[2] === undefined ? 0 : Number(data[z].current[2]);
+          if (Number.isNaN(data[z].current[2])) {
+            data[z].current[2] = 0;
+          }
+          return z;
+        })
+        .sort(function(a, b) {
+          return data[a].current[2] < data[b].current[2];
+        })
+        .filter(function(x) {
+          return data[x].current[0] === search[0] && data[x].current[1] === search[1];
+        })
+        .reduce(function(prev, w) {
+          if (prev !== undefined) {
+            return prev;
+          }
+          if (search[2] === undefined) {
+            return w;
+          }
+          if (data[w].current[0] === search[0] && data[w].current[1] === search[1] 
+            && data[w].current[2] === Number(search[2])) {
+            return w;
+          }
+          return;
+        }, undefined);
 
-      var search = reqSubdomain[0].split('-');
-
-      var found = Object.keys(data)
-      .map(function(z) {
-        data[z].current = data[z].technicalName.split('-');
-        data[z].current[2] = data[z].current[2] === undefined ? 0 : Number(data[z].current[2]);
-        if (Number.isNaN(data[z].current[2])) {
-          data[z].current[2] = 0;
+        if (found !== undefined) {
+          var url = 'http://'+found+':'+ '3000';
+          debug('reverseproxy#1.1.1', url);
+          proxy.web(req, res, { target: url });
+          proxy.on('error', function(e) {
+            console.error('reverseproxy#1.1.2', e);
+            next(new Error('Bad gateway'));
+          });
+          return;
+        } else {
+          debug('reverseproxy#1.2');
+          res.render('404', { title: 'No any app found :( !', path: '/', userName: req.user });
         }
-        return z;
-      })
-      .sort(function(a, b) {
-        return data[a].current[2] < data[b].current[2];
-      })
-      .filter(function(x) {
-        return data[x].current[0] === search[0] && data[x].current[1] === search[1];
-      })
-      .reduce(function(prev, w) {
-        if (prev !== undefined) {
-          return prev;
-        }
-        if (search[2] === undefined) {
-          return w;
-        }
-        if (data[w].current[0] === search[0] && data[w].current[1] === search[1] && data[w].current[2] === Number(search[2])) {
-          return w;
-        }
-        return;
-      }, undefined);
-
-      debug("########## FOUND : " + found + " ##########");
-
-      if (found !== undefined) {
-        var url = 'http://'+found+':'+ '3000';
-        debug('reverseproxy#1.1.1', url);
-        proxy.web(req, res, { target: url });
-        proxy.on('error', function(e) {
-          console.error("reverseproxy#1.1.2", e);
-          next(new Error('Bad gateway'))
-        } );
-        return;
+      } else {
+        debug('reverseproxy#1.0');
+        return next();
       }
-      else {
-        debug('reverseproxy#1.2');
-        res.render('404', { title: 'No any app found :( !', path: '/', userName: req.user });
-      }
-    }
-    else {
-      debug('reverseproxy#1.0');
-      next();
-    }
-    })
-  }
+    });
+  };
 
-}
+};
