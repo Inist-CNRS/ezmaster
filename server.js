99d41c8dcf2a7609981f0be321aaae4a74a75a93
githosting <alexandrebettinger@hotmail.fr>
2016-06-29 13:40:34 +0200

    Test heartbeat on vilodex.

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ module.exports = function(config, start) {
       // THE HEARTBEATS HEART
 
         // Repeat every 5000 milliseconds = every 5 seconds.
-        var heart1 = heartbeats.createHeart(5000);
+        var heart1 = heartbeats.createHeart(10000);
 
         // For infinite repeat we use {repeat : 0}.
-        heart1.createEvent(1, function(heartbeat, last){
+        heart1.createEvent(1, {repeat : 0}, function(heartbeat, last){
 
-          // Instructions done on each Heartbeats heart beat.
+          // Instructions done on each heart beat.
 
   console.log("########## BEAT ! ##########");
 
