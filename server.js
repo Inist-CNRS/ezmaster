2aa67bc7d74f6a49b842a97ef421951ed6273a55
githosting <alexandrebettinger@hotmail.fr>
2016-06-29 14:27:00 +0200

    Test heartbeat on vilodex.

--- server.js
+++ server.js
@@ -30,53 +30,51 @@ module.exports = function(config, start) {
     // Use socket.io on the server.
     var io = require('socket.io').listen(server);
 
+    var heartAlreadyCreated = false;
 
-
-
-
-
-      // Caching the instances list to have a reference for comparisons to come.
-        var cacheInstances = {};
-
-      // THE HEARTBEATS HEART
-
-        // Repeat every 5000 milliseconds = every 5 seconds.
-        var heart1 = heartbeats.createHeart(5000);
-
-        // For infinite repeat we use {repeat : 0}.
-        heart1.createEvent(1, {repeat : 0}, function(heartbeat, last){
-
-          // Instructions done on each heart beat.
-
-            console.log("########## BEAT ! ##########");
-
-            instances.getInstances(function(err,beatInstances){
-
-              // If there are some differences between cacheInstances (reference) and beatInstances (just get) :
-                // Update cacheInstances with beatInstances.
-                // Broadcast to all users this new version to update the 'containers' variable in template.html.
-              if(!(JSON.stringify(cacheInstances) === JSON.stringify(beatInstances) )) {
-
-                cacheInstances = beatInstances;
-                io.broadcast.emit('refreshInstances', beatInstances);
-
-              }
-
-            });
-
-        });
-
-
-
-
-
-
-
+    // Caching the instances list to have a reference for comparisons to come.
+    var cacheInstances = {};
 
 
     io.sockets.on('connection', function (socket){
 
+      if(!heartAlreadyCreated) {
 
+
+
+        heartAlreadyCreated = true;
+
+
+
+        // THE HEARTBEATS HEART
+
+          // Repeat every 5000 milliseconds = every 5 seconds.
+          var heart1 = heartbeats.createHeart(5000);
+
+          // For infinite repeat we use {repeat : 0}.
+          heart1.createEvent(1, {repeat : 0}, function(heartbeat, last){
+
+            // Instructions done on each heart beat.
+
+              console.log("########## BEAT ! ##########");
+
+              instances.getInstances(function(err,beatInstances){
+
+                // If there are some differences between cacheInstances (reference) and beatInstances (just get) :
+                  // Update cacheInstances with beatInstances.
+                  // Broadcast to all users this new version to update the 'containers' variable in template.html.
+                if(!(JSON.stringify(cacheInstances) === JSON.stringify(beatInstances) )) {
+
+                  cacheInstances = beatInstances;
+                  socket.broadcast.emit('refreshInstances', beatInstances);
+
+                }
+
+              });
+
+          });
+
+      }
 
     });
 
