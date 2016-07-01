86ff8723ed2381a68df819d4c4d621564fdfa2ad
JulienCoyard <julien.coyard@gmail.com>
2016-07-01 14:43:26 +0200

    rename of test.js

---
+++ test/testRoute.js
@@ -0,0 +1,28 @@
+/* global describe, it */
+'use strict';
+
+// Travis run this file while building.
+var request = require('supertest');
+
+describe('the ezmaster application', function () {
+
+  request = request('http://127.0.0.1:35267');
+
+  describe('GET', function () {
+
+    this.timeout(5000);
+
+    it('index route', function (done) {
+      request.get('/').expect(200, function (err) {
+        done(err);
+      });
+    });
+
+    it('basic route', function (done) {
+      request.get('/-/v1/instances').expect(200, function (err) {
+        done(err);
+      });
+    });
+  });
+
+});
