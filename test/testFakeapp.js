0f40b57970ff97a8037ff8ebaf421d38c1a1733d
JulienCoyard <julien.coyard@gmail.com>
2016-07-05 09:46:27 +0200

    Change fir testfakeapp and Makefile

--- test/testFakeapp.js
+++ test/testFakeapp.js
@@ -3,51 +3,78 @@
 
 // Travis run this file while building.
 var execSync = require('child_process').execSync
-  , assert = require('chai').assert
-  , Docker = require('dockerode')
-  , docker = new Docker({ socketPath: '/var/run/docker.sock'});
+  , jsonfile = require('jsonfile')
+  , path = require('path')
+  , fs = require('fs')
+  , request = require('supertest');
 
 
-describe('Run fakeapp', function (cb) {
+//Build the fakeapp image
+// Create the container and the manifest file
+//Before all the test
+before(function() {
 
-  var dockerInstances = [];
+  var cmd = 'docker build -t fakeapp --build-arg https_proxy --build-arg http_proxy '
+            +__dirname+'/datasets/fakeapp/';
 
+  execSync(cmd);
+
+  //Docker command for creating the container
   var cmd = 'docker run -d -p 60000:3000 ' +
-            '-e http_proxy -e https_proxy -e EZMASTER_MONGODB_HOST_PORT '+
-            '--net=ezmaster_default --link ezmaster_db '+
-            '-v '+process.env.EZMASTER_PATH+'/test'
-            +'/datasets/fakeapp/:/opt/ezmaster/data/ '+
-            '--name fakeapp fakeapp';
+          '-e http_proxy -e https_proxy -e EZMASTER_MONGODB_HOST_PORT '+
+          '--net=ezmaster_default --link ezmaster_db '+
+          '-v '+process.env.EZMASTER_PATH+'/test'
+          +'/datasets/fakeapp/:/opt/ezmaster/data/ '+
+          '--name fakeapp fakeapp';
+
+  //Creating the manifest and writting the longName inside
+  var newlongName = {
+    'longName' : 'fakeapp'
+  };
+
+  jsonfile.writeFile(
+    path.join(__dirname, '../manifests/fakeapp.json')
+    , newlongName, function (err) {
+      if (err) { return new Error(err); }
+    });
 
   execSync(cmd);
 
 
-  docker.listContainers({ all : true }, function (err, containers) {
+});
 
-    if (err) { return cb(err); }
 
-    containers.forEach(function (data) {
-      dockerInstances.push(data.Names[0].split('/')[1]);
-    });
+describe('Test fakeapp', function (cb) {
 
-  });
+
+  request = request('http://127.0.0.1:35267');
+
 
   it('Found fakeapp', function (done) {
 
+    request.get('/-/v1/instances/').expect(function (res) {
+      if (!res.body.fakeapp) { return new Error('fakeapp not found!'); }
+    })
+    .end(done);
 
-    var found = false;
 
 
-    dockerInstances.forEach(function (instance) {
-
-      if (instance.toString() == 'fakeapp') {
-        found = true;
-      }
-    });
-    done(assert.equal(found, true));
-
-    var cmd = 'docker rm -f fakeapp';
-
-    execSync(cmd);
   });
+
+
+});
+
+
+//Delete the container and the manifest file after all the test have been done
+after(function() {
+  var cmd = 'docker rm -f fakeapp';
+  execSync(cmd);
+
+  fs.unlink(
+    path.join(__dirname, '../manifests/fakeapp.json')
+    , function (err) {
+      if (err) { return new Error(err); }
+    });
+
+
 });
