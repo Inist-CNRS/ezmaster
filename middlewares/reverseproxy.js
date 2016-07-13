5deebc12f690684d5701c63c389fbf7a25dbd006
githosting <alexandrebettinger@hotmail.fr>
2016-07-13 16:26:10 +0200

    Test RP.

--- middlewares/reverseproxy.js
+++ middlewares/reverseproxy.js
@@ -102,9 +102,54 @@ module.exports = function(options, core) {
         }
       }
       else if (isRpEnabled.byHost) {
+
         console.log("########## BY HOST ##########");
-        var url = 'http://'+host+':'+ '3000';
+
+        var search = reqSubdomain[0].split('-');
+
+        var found = Object.keys(instances)
+          .map(function(z) {
+            instances[z].current = instances[z].technicalName.split('-');
+            instances[z].current[2] = instances[z].current[2] === undefined
+              ? 0 : Number(instances[z].current[2]);
+            if (Number.isNaN(instances[z].current[2])) {
+              instances[z].current[2] = 0;
+            }
+            return z;
+          })
+          .sort(function (a, b) {
+            return instances[a].current[2] < instances[b].current[2];
+          })
+          .filter(function (x) {
+            return instances[x].current[0] === search[0] && instances[x].current[1] === search[1];
+          })
+          .reduce(function (prev, w) {
+            if (prev !== undefined) {
+              return prev;
+            }
+            if (search[2] === undefined) {
+              return w;
+            }
+            if (instances[w].current[0] === search[0] && instances[w].current[1] === search[1]
+              && instances[w].current[2] === Number(search[2])) {
+              return w;
+            }
+            return;
+          }, undefined);
+
+        if (found !== undefined) {
+          var url = 'http://'+found+':'+ '3000';
           proxy.web(req, res, { target: url });
+          proxy.on('error', function(e) {
+            console.error('reverseproxy#1.1.2', e);
+            next(new Error('Bad gateway'));
+          });
+          return;
+        }
+        else {
+          debug('reverseproxy#1.2');
+          res.render('404', { title: 'No any app found :( !', path: '/', userName: req.user });
+        }
       }
       else {
         console.log("########## BY ELSE ##########");
