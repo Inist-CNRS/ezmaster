abac48ff0e6ecde674c5396f2ea7093c6d54f58d
JulienCoyard <julien.coyard@gmail.com>
2016-08-31 15:56:00 +0200

    Add option -dt in docker run command

--- routes/v1.js
+++ routes/v1.js
@@ -370,7 +370,7 @@ module.exports = function (router, core) {
           portMax = freePortSplitted[0];
         }
 
-        var cmd = 'docker run -d -p '+portMax+':3000 ' +
+        var cmd = 'docker run -dt -p '+portMax+':3000 ' +
         '-e http_proxy -e https_proxy -e EZMASTER_MONGODB_HOST_PORT '+
         '--net=ezmaster_default --link ezmaster_db '+
         '-v '+process.env.EZMASTER_PATH+'/instances/'+
