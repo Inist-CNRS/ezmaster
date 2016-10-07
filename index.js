a93d53836161dea0d366bdf56fd019aec79557e3
Stéphane Gully <stephane.gully@gmail.com>
2016-10-07 22:39:04 +0200

    start of a very big refactoring related to expressjs/castor-core cleanup

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * EzMaster entrypoint
+ */
+
+/*eslint global-require:"warn"*/
+'use strict';
+
+var cfg   = require('./lib/config.js');
+var kuler = require('kuler');
+
+var express = require('express');
+var app    = express();
+var server = require('http').Server(app);
+var io     = require('socket.io')(server);
+
+app.use(express.static('public'));
+app.use('/', require('./routes/v1.js'));
+
+io.on('connection', function (socket) {
+  console.log('connexion')
+  cfg.socket = socket;
+});
+
+server.listen(cfg.EZMASTER_PORT, function (err) {
+  console.info(kuler(cfg.package.name + ' ' + cfg.package.version + ' is listening.', 'olive'));
+  console.info(kuler(cfg.baseURL + '/', 'limegreen'));
+});
