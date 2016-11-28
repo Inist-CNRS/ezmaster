1edd2d44e7fb2bfad568c3b5066ab21df0392d93
Yannick Schurter <yannick.schurter@gmail.com>
2016-11-28 17:26:53 +0100

    factorising big html blocs

--- public/src/components/table.vue
+++ public/src/components/table.vue
@@ -25,40 +25,24 @@
 
       <template v-for="item in containers">
 
-        <tr v-if="item.running" class="success" >
+        <tr v-bind:class="{ success: item.running, danger: !item.running }">
           <td>{{ item.longName }}</td>
           <td>{{ item.technicalName }}</td>
           <td>{{ item.creationDate }}</td>
           <td>{{ item.app }}</td>
-          <td> <span>Started</span> </td>
+          <td>
+            <span v-if="item.running">Started</span>
+            <span v-else>Stopped</span>
+          </td>
           <td class="actions">
-            <ul class="bread" :data-id="item.technicalName">
-              <li class="start" title="Start the instance."><button type='button' class="btn btn-raised btn-sm btn-success button" disabled>Start</button></li>
-              <li class="stop" title="Stop the instance."><button type='button' class="btn btn-raised btn-sm btn-danger button" v-on:click="stopInstance(item.containerId)">Stop</button></li>
+            <ul class="bread">
+              <li class="start" title="Start the instance."><button type='button' class="btn btn-raised btn-sm btn-success button" v-bind:disabled="item.running" v-on:click="startInstance(item.containerId)">Start</button></li>
+              <li class="stop" title="Stop the instance."><button type='button' class="btn btn-raised btn-sm btn-danger button" v-bind:disabled="!item.running" v-on:click="stopInstance(item.containerId)">Stop</button></li>
               <li class="delete" title="Delete the instance."><button type='button' class="btn btn-raised btn-sm btn-warning button" v-on:click="deleteInstance(item.containerId)">Delete</button></li>
               <li class="updateConfig" title="Update the instance configuration."><button type='button' class="btn btn-raised btn-sm btn-info button" v-on:click="displayConfig(item.containerId)">Config </button></li>
               <li class="updateData" title="Upload data for the instance."><button type='button' class="btn btn-raised btn-sm btn-info button" v-on:click="displayFormUpload(item.containerId)">Data</button></li>
-              <li class="openPublicLink" title="Open the instance."><a class="btn btn-raised btn-sm btn-link button publicLink" :target="item.target" :href="item.publicURL">Access</a></li>
+              <li class="openPublicLink" title="Open the instance."><a class="btn btn-raised btn-sm btn-link button publicLink" :target="item.target" :href="item.publicURL" v-bind:disabled="!item.running">Access</a></li>
               <li v-if="publicDomain != ''" class="openPublicLink" title="Open the instance."><a class="btn btn-raised btn-sm btn-link button publicLink"  :target="item.target[publicDomain]" :href="'http://' + item.target + '.' + publicDomain">Public Access </a></li>
-            </ul>
-          </td>
-        </tr>
-
-        <tr v-else class="danger" :data-id="item.containerId">
-          <td>{{ item.longName }}</td>
-          <td>{{ item.technicalName }}</td>
-          <td>{{ item.creationDate }}</td>
-          <td>{{ item.app }}</td>
-          <td><span>Stopped</span></td>
-          <td class="actions">
-            <ul class="bread" :data-id="item.technicalName">
-              <li class="start" title="Start the instance"><button type='button' class="btn btn-raised btn-sm btn-success button" v-on:click="startInstance(item.containerId)">Start</button></li>
-              <li class="stop" title="Stop the instance"><button type='button' class="btn btn-raised btn-sm btn-danger button" disabled>Stop</button></li>
-              <li class="delete" title="Delete the instance"><button type='button' class="btn btn-raised btn-sm btn-warning button" v-on:click="deleteInstance(item.containerId)">Delete</button></li>
-              <li class="updateConfig" title="Updating configuration of the instance"><button type='button' class="btn btn-raised btn-sm btn-info button" v-on:click="displayConfig(item.containerId)">Config</button></li>
-              <li class="updateData" title="Upload data for the instance."><button type='button' class="btn btn-raised btn-sm btn-info button" v-on:click="displayFormUpload(item.containerId)">Data</button></li>
-              <li class="openPublicLink" title="Open the instance"><button type="button" class="btn btn-raised btn-sm btn-link button" disabled>Access</button></li>
-              <li v-if="publicDomain != ''" class="openPublicLink" title="Open the instance"><button type="button" class="btn btn-raised btn-sm btn-link button" disabled>Public Access</button></li>
             </ul>
           </td>
         </tr>
@@ -103,12 +87,12 @@
           </div>
           <div class="panel-body">
             <div id="jsoneditor"></div>
-            <span id='spanConfigError' class='text-danger'></span>
+            <span id="spanConfigError" class="text-danger"></span>
           </div>
           <div class="panel-footer">
-            <a class="btn btn-default" v-on:click='cancelConfig' data-dismiss="modal">Cancel</a>
-            <a id='buttonUpdate' class="btn btn-info button-right" v-on:click="updateConfig">Update</a>
-            <a id='buttonUpdateDisable' class="btn btn-info button-update" disabled>Update</a>
+            <a class="btn btn-default" v-on:click="cancelConfig" data-dismiss="modal">Cancel</a>
+            <a id="buttonUpdate" class="btn btn-info button-right" v-on:click="updateConfig">Update</a>
+            <a id="buttonUpdateDisable" class="btn btn-info button-update" disabled>Update</a>
           </div>
         </div>
       </div>
