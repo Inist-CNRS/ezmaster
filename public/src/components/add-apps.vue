dd32e5182fc690a73bfd42aaf0bf097eb06744a6
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-11-29 16:18:04 +0100

    :lint:

--- public/src/components/add-apps.vue
+++ public/src/components/add-apps.vue
@@ -5,64 +5,64 @@
     <div class="modal fade" id="modal-add-image"  tabindex="-1" role="dialog">
       <div class="modal-dialog">
         <div class="modal-content">
-            <form novalidate id="add-image-form" name="Form" class="form-horizontal">
-              <fieldset>
-                <div class="modal-header modal-addapp">
-                  <legend>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">×</button>
-                    <span class="titleFormAddInstance">Add Application</span>
-                  </legend>
-                </div>
-                <br />
-                <div class="modal-body">
+          <form novalidate id="add-image-form" name="Form" class="form-horizontal">
+            <fieldset>
+              <div class="modal-header modal-addapp">
+                <legend>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">×</button>
+                  <span class="titleFormAddInstance">Add Application</span>
+                </legend>
+              </div>
+              <br />
+              <div class="modal-body">
 
-                  <div class="form-group">
-                    <label for="inputImageName" class="col-md-3 control-label">Application Name</label>
-                    <div class="col-md-9">
-                      <input class="form-control sizeInput"
-					         id="inputImageName"
-							 name="inputImageName"
-							 placeholder="inistcnrs/ezvis"
-							 type="text"
-							 v-model="imageName"
-							 v-on:blur.stop="searchTags"
-							 >
-					  <select class="form-control sizeInput" v-bind:disabled="isSearchingTags" id="inputVersionImage" v-model="versionImage">
-						<option v-for="(version, index) in applicationVersions">
-							{{ version.name }}
-						</option>
-					  </select>
-                    </div>
-
-                    <button type="button" class="btn btn-default btn-md" v-on:click='showSettings'>
-                      <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Settings
-                    </button>
-
-
-                    <div class="form-group" id='settings' v-show="show">
-                      <label for="inputImageHub" class="col-md-3 control-label">Docker registry</label>
-                      <div class="col-md-9">
-                        <input class="form-control" id="inputImageHub" name="inputImageHub" placeholder="vsregistry.intra.inist.fr:5000" type="text" v-model="imageHub">
-                      </div>
-
-                      <label for="inputUserName" class="col-md-3 control-label">UserName</label>
-                      <div class="col-md-9">
-                        <input class="form-control sizeInput" id="inputUserName" name="inputUserName" placeholder="username" type="text" v-model="username">
-                      </div>
-
-                      <label for="inputImageHub" class="col-md-3 control-label">Password</label>
-                      <div class="col-md-9">
-                        <input class="form-control sizeInput" id="inputPassword" name="inputPassword" placeholder="password" type="password" v-model="password">
-                      </div>
-
-                      <label for="inputEmail" class="col-md-3 control-label">Email</label>
-                      <div class="col-md-9">
-                        <input class="form-control sizeInput" id="inputEmail" name="inputEmail" placeholder="test@email.com" type="text" v-model="email">
-
-                      </div>
-
-                    </div>
+                <div class="form-group">
+                  <label for="inputImageName" class="col-md-3 control-label">Application Name</label>
+                  <div class="col-md-9">
+                    <input class="form-control sizeInput"
+                           id="inputImageName"
+                           name="inputImageName"
+                           placeholder="inistcnrs/ezvis"
+                           type="text"
+                           v-model="imageName"
+                           v-on:blur.stop="searchTags"
+                           >
+                           <select class="form-control sizeInput" v-bind:disabled="isSearchingTags" id="inputVersionImage" v-model="versionImage">
+                             <option v-for="(version, index) in applicationVersions">
+                             {{ version.name }}
+                             </option>
+                           </select>
                   </div>
+
+                  <button type="button" class="btn btn-default btn-md" v-on:click='toggleSettings'>
+                    <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Settings
+                  </button>
+
+
+                  <div class="form-group" id='settings' v-show="show">
+                    <label for="inputImageHub" class="col-md-3 control-label">Docker registry</label>
+                    <div class="col-md-9">
+                      <input class="form-control" id="inputImageHub" name="inputImageHub" placeholder="vsregistry.intra.inist.fr:5000" type="text" v-model="imageHub">
+                    </div>
+
+                    <label for="inputUserName" class="col-md-3 control-label">UserName</label>
+                    <div class="col-md-9">
+                      <input class="form-control sizeInput" id="inputUserName" name="inputUserName" placeholder="username" type="text" v-model="username">
+                    </div>
+
+                    <label for="inputImageHub" class="col-md-3 control-label">Password</label>
+                    <div class="col-md-9">
+                      <input class="form-control sizeInput" id="inputPassword" name="inputPassword" placeholder="password" type="password" v-model="password">
+                    </div>
+
+                    <label for="inputEmail" class="col-md-3 control-label">Email</label>
+                    <div class="col-md-9">
+                      <input class="form-control sizeInput" id="inputEmail" name="inputEmail" placeholder="test@email.com" type="text" v-model="email">
+
+                    </div>
+
+                  </div>
+                </div>
 
 
 
@@ -74,142 +74,139 @@
                     <img id="loaderAddInstance" src="/img/ajax-loader.gif" alt="Loading"/><br />
                     <span class="text-primary" id="messageLoaderAddInstance">{{ statusPull }}</span>
                   </div>
-                   <div id='errorLoaderImages' class="loader">
+                  <div id='errorLoaderImages' class="loader">
                     <span class="text-danger" id="errorLoaderAddImage">An error {{ codeErrorPull }} was received : {{ messageErrorPull }}.</span><br />
-					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                   </div>
 
                 </div>
-              </fieldset>
-            </form>
+            </fieldset>
+          </form>
+              </div>
         </div>
       </div>
     </div>
-  </div>
 
 </template>
 
 
 <script>
-  /* global io */
-  var socket = io()
+/* global io */
+var socket = io();
 
-  export default {
+export default {
 
-    mounted () {
-      var self = this
-      var fullFsPercent
+  mounted () {
+    var self = this;
+    var fullFsPercent;
 
-      socket.on('statusPull', function (infoPull) {
-        self.statusPull = infoPull
-      })
+    socket.on('statusPull', function (infoPull) {
+      self.statusPull = infoPull;
+    })
 
-      self.$http.get('/-/v1/config').then(function (result) {
-        var config = result.data
-        self.fullFsPercent = config.fullFsPercent
-        fullFsPercent = config.fullFsPercent
-      }, console.error)
+    self.$http.get('/-/v1/config').then(function (result) {
+      var config = result.data;
+      self.fullFsPercent = config.fullFsPercent;
+      fullFsPercent = config.fullFsPercent;
+    }, console.error)
 
-      socket.on('refreshInfosMachine', function (infosMachineSocket) {
-        // Update variable 'infosMachine'.
-        // This will automatically refresh the infosMachineTable component.
-        self.infosMachine = infosMachineSocket
-        // Put this bool to true in order to avoid console error on infosMachine component launch.
-        // This bool is used on the HTML code just above.
-        self.boolInfosFeed = true
-        self.disableAddButton = fullFsPercent < infosMachineSocket.useDiskPercentage
-      })
-    },
+    socket.on('refreshInfosMachine', function (infosMachineSocket) {
+      // Update variable 'infosMachine'.
+      // This will automatically refresh the infosMachineTable component.
+      self.infosMachine = infosMachineSocket;
+      // Put this bool to true in order to avoid console error on infosMachine component launch.
+      // This bool is used on the HTML code just above.
+      self.boolInfosFeed = true;
+      self.disableAddButton = fullFsPercent < infosMachineSocket.useDiskPercentage;
+    })
+  },
 
-    methods: {
+  methods: {
 
-	  searchTags : function() {
-		const self = this;
-		if (self.isSearchingTags === true) {
-			return
-		}
-		self.isSearchingTags = true;
-		console.log('searchTags for', self.imageName);
-		self.applicationVersions = [];
-		if (self.imageName.search(/\w\/\w/) >= 0) {
-			const url = '/-/v1/hub/repositories/' + self.imageName.trim() + '/tags/?page=1&page_size=5'
-			console.log('fecth', url);
-			self.applicationVersions = [];
-			self.$http.get(url).then((response) => {
-				self.cacheImageVersions[self.imageName] = true;
-				if (response.body && response.body.results && Array.isArray(response.body.results)) {
-					response.body.results.forEach(function(item) {
-						self.$set(self.applicationVersions, self.applicationVersions.length, item)
-					})
-					}
-				self.isSearchingTags = false;
-			}, (response) => {
-			self.isSearchingTags = false;
-			})
-		}
-		else {
-			self.isSearchingTags = false;
-		}
-	  },
-      addImage: function (event) {
-        var self = this
-
-        document.getElementById('loaderImage').style.display = 'block'
-
-        var formdata = {
-          imageName: this.imageName,
-          versionImage: this.versionImage,
-          imageHub: this.imageHub,
-          username: this.username,
-          password: this.password,
-          email: this.email
-        }
-
-        this.$http.post('/-/v1/app', formdata).then(function (result) {
-          if (result.status === 200) {
-            $('#modal-add-image').modal('hide')
-			console.log('emit', 'refreshApplicationsList')
-            self.$emit('refreshApplicationsList')
+    searchTags: function () {
+      const self = this;
+      if (self.isSearchingTags === true) {
+        return
+      }
+      self.isSearchingTags = true;
+      self.applicationVersions = [];
+      if (self.imageName.search(/\w\/\w/) >= 0) {
+        const url = '/-/v1/hub/repositories/' + self.imageName.trim() + '/tags/?page=1&page_size=5';
+        self.applicationVersions = [];
+        self.$http.get(url).then((response) => {
+          self.cacheImageVersions[self.imageName] = true;
+          if (response.body && response.body.results && Array.isArray(response.body.results)) {
+            response.body.results.forEach(function (item) {
+              self.$set(self.applicationVersions, self.applicationVersions.length, item)
+            })
           }
-        }, function (error) {
-          if (error) {
-            this.codeErrorPull = error.status
-            this.messageErrorPull = error.data
-            document.getElementById('loaderImage').style.display = 'none'
-            document.getElementById('errorLoaderImages').style.display = 'block'
-          }
+          self.isSearchingTags = false;
+        }, (response) => {
+          self.isSearchingTags = false;
         })
-      },
+      }
+      else {
+        self.isSearchingTags = false;
+      }
+    },
+    addImage: function (event) {
+      var self = this;
 
-      showSettings: function (event) {
-		this.show = this.show === false ? true : false
+      document.getElementById('loaderImage').style.display = 'block';
+
+      var formdata = {
+        imageName: this.imageName,
+        versionImage: this.versionImage,
+        imageHub: this.imageHub,
+        username: this.username,
+        password: this.password,
+        email: this.email
       }
 
+      this.$http.post('/-/v1/app', formdata).then(function (result) {
+        if (result.status === 200) {
+          $('#modal-add-image').modal('hide');
+          self.$emit('refreshApplicationsList');
+        }
+      }, function (error) {
+        if (error) {
+          this.codeErrorPull = error.status;
+          this.messageErrorPull = error.data;
+          document.getElementById('loaderImage').style.display = 'none';
+          document.getElementById('errorLoaderImages').style.display = 'block';
+        }
+      })
     },
 
-    data () {
-      return {
-        imageName: '',
-        versionImage: '',
-		cacheImageVersions: {},
-        messageErrorPull: '',
-        imageHub: '',
-        email: '',
-        password: '',
-        username: '',
-        show: false,
-        statusPull: '',
-        codeErrorPull: '',
-        infosMachine: {},
-        fullFsPercent: '',
-        disableAddButton: false,
-        boolInfosFeed: false,
-		applicationVersions: [],
-		isSearchingTags: false
-      }
+    toggleSettings: function (event) {
+      this.show = !this.show;
     }
 
+  },
+
+  data () {
+    return {
+      imageName: '',
+      versionImage: '',
+      cacheImageVersions: {},
+      messageErrorPull: '',
+      imageHub: '',
+      email: '',
+      password: '',
+      username: '',
+      show: false,
+      statusPull: '',
+      codeErrorPull: '',
+      infosMachine: {},
+      fullFsPercent: '',
+      disableAddButton: false,
+      boolInfosFeed: false,
+      applicationVersions: [],
+      isSearchingTags: false
+    }
   }
+
+}
 
 </script>
 
@@ -217,14 +214,14 @@
 
 <style scoped>
 
-  #add_image {
-    margin-left: 2.5%;
-    background-color:#0277BD;
-  }
+#add_image {
+  margin-left: 2.5%;
+  background-color:#0277BD;
+}
 
   .sizeInput {
     width : 32%;
-    display: inline-block;
+  display: inline-block;
   }
 
   .block-input {
@@ -232,11 +229,11 @@
   }
 
   #urlPreview{
-      background-color:       #CFD8DC;
+    background-color:       #CFD8DC;
   }
 
   .titleFormAddInstance {
-      color:white;
+    color:white;
   }
 
   .modal-addapp{
--- routes/v1-hub.js
+++ routes/v1-hub.js
@@ -2,12 +2,11 @@
 /*eslint no-sync: "off", global-require: "off"*/
 'use strict';
 
-var cfg = require('../lib/config.js')
-  , path = require('path')
-  , url = require('url')
+var path = require('path')
   , basename = path.basename(__filename, '.js')
   , debug = require('debug')('ezmaster:' + basename)
   , request = require('request')
+  , url = require('url')
   ;
 
 
@@ -19,7 +18,7 @@ router.route('/*').get(function (req, res, next) {
   var originalUrl = url.parse(req.originalUrl.replace('/-/v1/hub', '/v2'));
 
   originalUrl.protocol = 'http';
-  originalUrl.host = 'hub.docker.com'
+  originalUrl.host = 'hub.docker.com';
 
   request.get({
     url : url.format(originalUrl),
