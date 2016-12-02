01711e807306134f49426a08a2d73061a210e08b
François Parmentier <francois.parmentier@gmail.com>
2016-12-02 17:14:02 +0100

    :rotating_light: Make diskusage test pass

--- test/diskusage.js
+++ test/diskusage.js
@@ -11,12 +11,19 @@ describe('the ezmaster diskusage lib', function () {
     dusage(function (err, result) {
       if (err) return done(err);
 
-      expect(result).to.have.property('freeDiskRaw');
-      expect(result).to.have.property('totalDiskRaw');
-      expect(result).to.have.property('freeDisk');
-      expect(result).to.have.property('totalDisk');
-      expect(result).to.have.property('useDiskPercentage');
       expect(result).to.have.property('fsIsAlmostFilled');
+      expect(result).to.have.property('diskApp');
+      expect(result).to.have.property('diskDocker');
+      expect(result.diskApp).to.have.property('freeDiskRaw');
+      expect(result.diskApp).to.have.property('totalDiskRaw');
+      expect(result.diskApp).to.have.property('freeDisk');
+      expect(result.diskApp).to.have.property('totalDisk');
+      expect(result.diskApp).to.have.property('useDiskPercentage');
+      expect(result.diskDocker).to.have.property('freeDiskRaw');
+      expect(result.diskDocker).to.have.property('totalDiskRaw');
+      expect(result.diskDocker).to.have.property('freeDisk');
+      expect(result.diskDocker).to.have.property('totalDisk');
+      expect(result.diskDocker).to.have.property('useDiskPercentage');
 
       done();
     });
