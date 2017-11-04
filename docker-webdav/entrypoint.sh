c8c10d62dcc024f93835f1ba02020984c3574ec5
Stéphane Gully <stephane.gully@gmail.com>
2017-11-04 12:05:41 +0100

    webdav: fix the "not allow to DELETE or PUT"

--- docker-webdav/entrypoint.sh
+++ docker-webdav/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eux
 
+# auth stuff
 if [ -n "${WEBDAV_USERNAME:-}" ] && [ -n "${WEBDAV_PASSWORD:-}" ]; then
     htpasswd -bc /etc/nginx/webdavpasswd $WEBDAV_USERNAME $WEBDAV_PASSWORD
 else
@@ -8,10 +9,4 @@ else
     sed -i 's%auth_basic_user_file webdavpasswd;% %g' /etc/nginx/nginx.conf
 fi
 
-# adjust the UID/GID stuff to allow webdav to write into the /media shared folder
-THE_UID=$(ls -ld /media/ | awk '{print $3}')
-THE_GID=$(ls -ld /media/ | awk '{print $4}')
-chmod go+w /dev/stderr /dev/stdout
-gosu $THE_UID mkdir -p /media/.tmp
-
-exec gosu $THE_UID "$@"
+exec gosu root "$@"
