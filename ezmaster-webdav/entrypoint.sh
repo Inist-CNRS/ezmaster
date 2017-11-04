939afe788e5016095fe44f51dc075ca4d0fc936c
Stéphane Gully <stephane.gully@gmail.com>
2017-11-04 18:34:53 +0100

    refactoring ezmaster-webdav

--- ezmaster-webdav/entrypoint.sh
+++ ezmaster-webdav/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eux
 
 # auth stuff
-if [ -n "${WEBDAV_USERNAME:-}" ] && [ -n "${WEBDAV_PASSWORD:-}" ]; then
-    htpasswd -bc /etc/nginx/webdavpasswd $WEBDAV_USERNAME $WEBDAV_PASSWORD
+if [ -n "${EZMASTER_USER:-}" ] && [ -n "${EZMASTER_PASSWORD:-}" ]; then
+    htpasswd -bc /etc/nginx/webdavpasswd $EZMASTER_USER $EZMASTER_PASSWORD
 else
     echo "No htpasswd config done"
     sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/nginx.conf
