5006ebfd6d931ba7181f5c2987f80b3542a4883e
Stéphane Gully <stephane.gully@gmail.com>
2017-11-05 12:30:39 +0100

    add a index.html to ezmaster-rp

--- ezmaster-api/index.js
+++ ezmaster-api/index.js
@@ -51,12 +51,14 @@ require('./lib/docker-websocket.js').init(io);
 
 server.listen(cfg.EZMASTER_PORT, function () {
   console.info(kuler(cfg.package.name + ' ' + cfg.package.version + ' is listening.', 'olive'));
-  console.info('EzMaster IHM:    ' +
+  console.info('ezmaster-front (backoffice IHM):       ' +
                kuler('http://' + cfg.publicIP + ':35268/', 'limegreen'));
-  console.info('EzMaster API:    ' +
+  console.info('ezmaster-api:                          ' +
                kuler('http://' + cfg.publicIP + ':35269/-/v1/', 'limegreen'));
-  console.info('EzMaster Webdav: ' +
+  console.info('ezmaster-webdav:                       ' +
                kuler('http://' + cfg.publicIP + ':35270/', 'limegreen'));
+  console.info('ezmaster-rp (instances reverse proxy): ' +
+               kuler('http://' + cfg.publicIP + ':35267/', 'limegreen'));
   if (cfg.publicDomain) {
     console.info('EzMaster reverse proxy: ' +
                  kuler('http://*.' + cfg.publicDomain + '/', 'limegreen'));
---
+++ ezmaster-rp/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -eux
+
+# here is a place to overload things like authentication stuff
+# or ...
+
+exec "$@"
