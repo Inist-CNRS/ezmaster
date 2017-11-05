499d319feb9093bb4f8b4916743505f394b4354c
Stéphane Gully <stephane.gully@gmail.com>
2017-11-05 11:28:44 +0100

    remove jsDAV stuff (replaced by ezmaster-webdav)

--- ezmaster-api/index.js
+++ ezmaster-api/index.js
@@ -17,7 +17,6 @@ var cors      = require('cors');
 var basicAuth = require('basic-auth-connect');
 
 var instances = require('./lib/instances.js');
-var DAV       = require('jsDAV/lib/jsdav');
 
 
 app.use(cors()); // to allow ezmaster-front to call it from client side
@@ -39,39 +38,6 @@ app.use('/-/v1/config',    require('./routes/v1-config.js'));
 app.use('/-/v1/app',       require('./routes/v1-app.js'));
 app.use('/-/v1/hub',       require('./routes/v1-hub.js'));
 app.use('/-/v1/instances', require('./routes/v1-instances.js'));
-
-// connect the webdav stuff (to be removed in ezmaster 4.0)
-app.use(function (req, res, next) {
-
-  if (req.url.search(/^\/wd--/) >= 0) {
-    var instanceId = req.url.match('wd--([^/]+)')[1];
-
-    instances.checkInstance(instanceId, function(err, container, data, manifest) {
-
-      if (err) { return next(err); }
-
-      var splittedName = data.Name.split('/');
-      debug('url', req.url);
-      DAV.mount({
-        node: cfg.dataInstancesPath + '/' + splittedName[1] + '/data',
-        mount: '/wd--' + instanceId,
-        server: req.app,
-        standalone: false
-      }).exec(req, res);
-
-
-    });
-
-  }
-  else {
-    return next();
-  }
-  /*
-  */
-});
-
-
-
 
 // load socket.io connections
 // penser à gérer le multi-utilisateur car la on ne communique qu'au dernier client
