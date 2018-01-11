d61ba7e7fde841a2bc9177621068cc6f4470db45
Stéphane Gully <stephane.gully@gmail.com>
2018-01-12 00:01:07 +0100

    fix socket.io bug when using ezmaster through the public interface

--- ezmaster-front/public/src/components/store.js
+++ ezmaster-front/public/src/components/store.js
@@ -1,6 +1,6 @@
 /* global io */
 module.exports = {
   ezMasterAPI: '',
-  socket: io(document.location.protocol + '//' + document.location.hostname + ':' + 35269),
+  socket: io(),
   applications: []
 };
