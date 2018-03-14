6efef30c6803de79ba9f42d45a3186ad60108165
Stéphane Gully <stephane.gully@gmail.com>
2018-03-14 20:58:43 +0100

    simplify css

--- ezmaster-front/src/EzMasterInstanceRow.js
+++ ezmaster-front/src/EzMasterInstanceRow.js
@@ -14,40 +14,32 @@ class EzMasterInstanceRow extends Component {
         <td>2018/03/05 14:46:05</td>
         <td>inistcnrs/lodex:8.18.0</td>
         <td>
-          <Badge color="danger">Stopper</Badge>
+          <Badge color="danger">Stopped</Badge>
+          <Badge color="success">Running</Badge>
         </td>
         <td>
-          <Button color="link" className="ezmaster-action ezmaster-action-stop">
+          <Button color="link" className="ezmaster-a ezmaster-a-stop">
             <i className="fa fa-stop-circle" />
           </Button>
-          <Button color="link" className="ezmaster-action ezmaster-action-play">
+          <Button color="link" className="ezmaster-a ezmaster-a-play">
             <i className="fa fa-play-circle" />
           </Button>
-          <Button
-            color="link"
-            className="ezmaster-action ezmaster-action-trash"
-          >
+          <Button color="link" className="ezmaster-a ezmaster-a-trash">
             <i className="fa fa-trash" />
           </Button>
-          <Button color="link" className="ezmaster-action ezmaster-action-cog">
+          <Button color="link" className="ezmaster-a ezmaster-a-cog">
             <i className="fa fa-cog" />
           </Button>
-          <Button
-            color="link"
-            className="ezmaster-action ezmaster-action-upload"
-          >
+          <Button color="link" className="ezmaster-a ezmaster-a-upload">
             <i className="fa fa-upload" />
           </Button>
-          <Button color="link" className="ezmaster-action ezmaster-action-link">
+          <Button color="link" className="ezmaster-a ezmaster-a-link">
             <i className="fa fa-link" />
           </Button>
-          <Button
-            color="link"
-            className="ezmaster-action ezmaster-action-globe"
-          >
+          <Button color="link" className="ezmaster-a ezmaster-a-globe">
             <i className="fa fa-globe" />
           </Button>
-          <Button color="link" className="ezmaster-action ezmaster-action-file">
+          <Button color="link" className="ezmaster-a ezmaster-a-file">
             <i className="fa fa-file" />
           </Button>
         </td>
