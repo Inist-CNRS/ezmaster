412fde29967525583a36c7f0ba0aab44fc419a9d
Stéphane Gully <stephane.gully@gmail.com>
2018-03-14 14:50:54 +0100

    prettier

--- ezmaster-front/src/App.js
+++ ezmaster-front/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from        "./logo.svg";
 import "./App.css";
 import { Button, Popover, PopoverHeader, PopoverBody } from "reactstrap";
 import { Container, Row, Col } from "reactstrap";
