3f4529511521440d49c26bfce61820dc6133cda4
Stéphane Gully <stephane.gully@gmail.com>
2018-03-14 12:09:14 +0100

    first bootstrap header

--- ezmaster-front/src/App.js
+++ ezmaster-front/src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Popover, PopoverHeader, PopoverBody } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 class App extends Component {
   constructor(props) {
@@ -21,38 +35,105 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <Button id="Popover1" onClick={this.toggle}>
-            Launch Popover
-          </Button>
-          <Popover
-            placement="bottom"
-            isOpen={this.state.popoverOpen}
-            target="Popover1"
-            toggle={this.toggle}
-          >
-            <PopoverHeader>Popover Title</PopoverHeader>
-            <PopoverBody>
-              Sed posuere consectetur est at lobortis. Aenean eu leo quam.
-              Pellentesque ornare sem lacinia quam venenatis vestibulum.
-            </PopoverBody>
-          </Popover>
-        </div>
-        <div>
-          {/* to test font-awesome with reactjs */}
-          <i className="fa fa-user fa-4x" />
-          &nbsp;
-          <i className="fa fa-github-square fa-4x" />
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Navbar color="faded" light expand="md">
+              <NavbarBrand href="/">EzMaster</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/components/">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="https://github.com/reactstrap/reactstrap">
+                      Github
+                    </NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Options
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>Option 1</DropdownItem>
+                      <DropdownItem>Option 2</DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>Reset</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div>
+              <Button id="Popover1" onClick={this.toggle}>
+                Launch Popover
+              </Button>
+              <Popover
+                placement="bottom"
+                isOpen={this.state.popoverOpen}
+                target="Popover1"
+                toggle={this.toggle}
+              >
+                <PopoverHeader>Popover Title</PopoverHeader>
+                <PopoverBody>
+                  Sed posuere consectetur est at lobortis. Aenean eu leo quam.
+                  Pellentesque ornare sem lacinia quam venenatis vestibulum.
+                </PopoverBody>
+              </Popover>
+            </div>
+            <div>
+              {/* to test font-awesome with reactjs */}
+              <i className="fa fa-user fa-4x" />
+              &nbsp;
+              <i className="fa fa-github-square fa-4x" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>.col</Col>
+          <Col>.col</Col>
+          <Col>.col</Col>
+          <Col>.col</Col>
+        </Row>
+        <Row>
+          <Col xs="3">.col-3</Col>
+          <Col xs="auto">.col-auto - variable width content</Col>
+          <Col xs="3">.col-3</Col>
+        </Row>
+        <Row>
+          <Col xs="6">.col-6</Col>
+          <Col xs="6">.col-6</Col>
+        </Row>
+        <Row>
+          <Col xs="6" sm="4">
+            .col-6 .col-sm-4
+          </Col>
+          <Col xs="6" sm="4">
+            .col-6 .col-sm-4
+          </Col>
+          <Col sm="4">.col-sm-4</Col>
+        </Row>
+        <Row>
+          <Col sm={{ size: 6, order: 2, offset: 1 }}>
+            .col-sm-6 .col-sm-order-2 .col-sm-offset-2
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12" md={{ size: 8, offset: 2 }}>
+            .col-sm-12 .col-md-6 .col-md-offset-3
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{ size: "auto", offset: 1 }}>.col-sm .col-sm-offset-1</Col>
+          <Col sm={{ size: "auto", offset: 1 }}>.col-sm .col-sm-offset-1</Col>
+        </Row>
+      </Container>
     );
   }
 }
