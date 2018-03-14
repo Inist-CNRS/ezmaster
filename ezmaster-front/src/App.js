bf38cc068bce178257a26ee10a12704a9c124d91
Stéphane Gully <stephane.gully@gmail.com>
2018-03-14 11:32:30 +0100

    integrates a bootstrap v4 in the reactjs

--- ezmaster-front/src/App.js
+++ ezmaster-front/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+  
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +30,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Button id="Popover1" onClick={this.toggle}>
+            Launch Popover
+          </Button>
+          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+            <PopoverHeader>Popover Title</PopoverHeader>
+            <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+          </Popover>
+        </div>
         <div>
 
 {/* to test font-awesome with reactjs */}
--- ezmaster-front/src/index.js
+++ ezmaster-front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'font-awesome/css/font-awesome.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css'; 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
