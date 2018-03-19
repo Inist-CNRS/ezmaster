b5fe32d0ac331b278af2e16a8c98ad35a434ee09
Stéphane Gully <stephane.gully@gmail.com>
2018-03-19 19:51:27 +0100

    webdav doc

--- ezmaster-front/src/ModalWebdav.js
+++ ezmaster-front/src/ModalWebdav.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import { Container, Row, Col } from "reactstrap";
 
 import windowsDrive from "./ModalWebdav-windows-drive.png";
@@ -22,15 +23,43 @@ class ModalWebdav extends Component {
         <ModalBody>
           <Container>
             <Row>
-              <Col>Windows bla bla</Col>
+              <Col>
+                <h3>Webdav and windows</h3>
+                <ListGroup>
+                  <ListGroupItem>1. Create a new network drive</ListGroupItem>
+                  <ListGroupItem>
+                    2. As shown on the screenshot on the right, copy paste the
+                    folowing webdav URL into the "Dossier" field.
+                    <br />
+                    <code>http://vp-dpi.intra.inist.fr:23556/</code>
+                  </ListGroupItem>
+                  <ListGroupItem>coucou</ListGroupItem>
+                </ListGroup>
+              </Col>
               <Col>
                 <img src={windowsDrive} width="300px" />
               </Col>
             </Row>
             <Row>
-              <Col>Ubuntu bla bla</Col>
               <Col>
-                <img src={windowsDrive} />
+                <hr />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h3>Webdav and ubuntu</h3>
+                <ListGroup>
+                  <ListGroupItem>1. Open your file explorer</ListGroupItem>
+                  <ListGroupItem>
+                    2. Type CTRL+L and enter this URL in the input field
+                    <br />
+                    <code>dav://vp-dpi.intra.inist.fr:23556/</code>
+                  </ListGroupItem>
+                  <ListGroupItem>coucou</ListGroupItem>
+                </ListGroup>
+              </Col>
+              <Col>
+                <img src={windowsDrive} width="300px" />
               </Col>
             </Row>
           </Container>
