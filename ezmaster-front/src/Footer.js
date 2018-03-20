a03ae6a34fdb517efe8b6ed89b23b0446aaf1b0f
Stéphane Gully <stephane.gully@gmail.com>
2018-03-20 21:17:45 +0100

    add the footer content

--- ezmaster-front/src/Footer.js
+++ ezmaster-front/src/Footer.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import { UncontrolledTooltip } from "reactstrap";
 import "./Footer.css";
 
 class Footer extends Component {
   render() {
-    return <footer>FOOTER</footer>;
+    return (
+      <footer>
+        <hr />
+        <div className="ezmaster-footer-text float-left ml-2">
+          <a href="https://github.com/Inist-CNRS/ezmaster">EzMaster</a> version
+          4.1.1
+        </div>
+        <div className="ezmaster-footer-icon float-right mr-2">
+          <a href="https://github.com/Inist-CNRS/ezmaster" className="mr-2">
+            <i className="fa fa-github" />
+          </a>
+          <UncontrolledTooltip placement="top" target=".fa-github">
+            EzMaster source code hosted on Github
+          </UncontrolledTooltip>
+
+          <a
+            href="https://trello.com/b/GCu64gDf/ezmaster-suivi-du-projet"
+            className="mr-2"
+          >
+            <i className="fa fa-trello" />
+          </a>
+          <UncontrolledTooltip placement="top" target=".fa-trello">
+            EzMaster developement tasks on Trello
+          </UncontrolledTooltip>
+
+          <a href="https://twitter.com/inist_ezmaster">
+            <i className="fa fa-twitter" />
+          </a>
+          <UncontrolledTooltip placement="top" target=".fa-twitter">
+            Follow EzMaster on Twitter
+          </UncontrolledTooltip>
+        </div>
+      </footer>
+    );
   }
 }
 
