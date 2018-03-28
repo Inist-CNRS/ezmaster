16fc06938504ac164a538b1f68a66eb3ac9527ef
Stéphane Gully <stephane.gully@gmail.com>
2018-03-28 21:11:01 +0200

    add the EZMASTER_UPLOAD_MAX_BODY_SIZE feature

--- ezmaster-rp/entrypoint.sh
+++ ezmaster-rp/entrypoint.sh
@@ -8,6 +8,11 @@ cp -f /etc/nginx/ezmaster-instance-nginx.conf.tpl /etc/nginx/conf.d/
 cp -f /etc/nginx/ezmaster-front-nginx.conf.tpl    /etc/nginx/conf.d/ 
 cp -f /etc/nginx/ezmaster-webdav-nginx.conf.tpl   /etc/nginx/conf.d/ 
 
+EZMASTER_UPLOAD_MAX_BODY_SIZE=${EZMASTER_UPLOAD_MAX_BODY_SIZE:-"500M"}
+sed -i "s#client_max_body_size 500M#client_max_body_size $EZMASTER_UPLOAD_MAX_BODY_SIZE#g" /etc/nginx/conf.d/ezmaster-front-nginx.conf.tpl
+sed -i "s#client_max_body_size 500M#client_max_body_size $EZMASTER_UPLOAD_MAX_BODY_SIZE#g" /etc/nginx/conf.d/ezmaster-webdav-nginx.conf.tpl
+sed -i "s#client_max_body_size 500M#client_max_body_size $EZMASTER_UPLOAD_MAX_BODY_SIZE#g" /etc/nginx/conf.d/ezmaster-instance-nginx.conf.tpl
+
 # create log folder if necessary
 mkdir -p /var/log/nginx/ezmaster-rp/instances/
 
