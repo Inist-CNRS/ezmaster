9c2ce5c0c52bd2bbf9c2bcb72e10fd280e21ebd9
Stéphane Gully <stephane.gully@gmail.com>
2018-04-30 14:00:33 +0200

    add "docker" in the description

--- ezmaster-front/src/Home.js
+++ ezmaster-front/src/Home.js
@@ -74,7 +74,7 @@ class Home extends Component {
               dangerouslySetInnerHTML={{
                 __html:
                   this.props.config.EZMASTER_HOME_DESCRIPTION ||
-                  'EzMaster is a non-technical backoffice tools for non-IT administrators. It aims to manage applications and instances. One instance of an application is a docker packaged software having its own config and its own data. Each instance is isolated and independant, it can be stopped <i class="fa fa-stop-circle"></i> or started <i class="fa fa-play-circle"></i> easily on demand. Applications are "ezmasterized" docker images, they can be easily downloaded <i class="fa fa-download"></i> from dockerhub.'
+                  'EzMaster is a non-technical docker backoffice tools for non-IT administrators. It aims to manage applications and instances. One instance of an application is a docker packaged software having its own config and its own data. Each instance is isolated and independant, it can be stopped <i class="fa fa-stop-circle"></i> or started <i class="fa fa-play-circle"></i> easily on demand. Applications are "ezmasterized" docker images, they can be easily downloaded <i class="fa fa-download"></i> from dockerhub.'
               }}
             />
           </Col>
