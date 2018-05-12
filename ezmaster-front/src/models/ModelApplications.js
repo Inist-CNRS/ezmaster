31784b23cc2ef106d57dbc4ed7d8eb4671a47bf4
Stéphane Gully <stephane.gully@gmail.com>
2018-05-12 11:49:16 +0200

    connect the get applications list to API

--- ezmaster-front/src/models/ModelApplications.js
+++ ezmaster-front/src/models/ModelApplications.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function fetchApplicationsList(cb) {
   axios
-    .get("/fakeapi/app.json")
+    .get("/-/v1/app")
     .then(response => {
       // data comming from AJAX request (ezmaster app stuff)
       let data = response.data;
