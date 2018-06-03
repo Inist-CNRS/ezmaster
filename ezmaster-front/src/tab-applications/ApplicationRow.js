cadbcfacc9572c67978ae8f88eae52bae9d96f11
Stéphane Gully <stephane.gully@gmail.com>
2018-06-03 22:47:32 +0200

    add a fist applications table list

---
+++ ezmaster-front/src/tab-applications/ApplicationRow.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import moment from "moment";
+import { UncontrolledTooltip } from "reactstrap";
+import { prettyBytes } from "../helpers.js";
+
+import "./ApplicationRow.css";
+
+class ApplicationRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.application.imageName}</td>
+        <td>
+          <span id={"creationDate-" + this.props.application.imageId}>
+            {moment(this.props.application.image.Created).fromNow()}
+          </span>
+          <UncontrolledTooltip
+            autohide={false}
+            placement="right"
+            target={"creationDate-" + this.props.application.imageId}
+          >
+            This application has been created exactly on{" "}
+            <code>{this.props.application.image.Created}</code>
+          </UncontrolledTooltip>
+        </td>
+        <td>{prettyBytes(this.props.application.image.Size)}</td>
+        <td>___</td>
+      </tr>
+    );
+  }
+}
+
+export default ApplicationRow;
--- ezmaster-front/src/tab-applications/Applications.js
+++ ezmaster-front/src/tab-applications/Applications.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-// import { Table } from "reactstrap";
-// import { Row, Col } from "reactstrap";
-// import { Button } from "reactstrap";
+import { Table } from "reactstrap";
+import { Row, Col } from "reactstrap";
+import { Button } from "reactstrap";
 import { Helmet } from "react-helmet";
+import shallowEqual from "fbjs/lib/shallowEqual";
 
 import "./Applications.css";
+import ApplicationRow from "./ApplicationRow.js";
 
 class Applications extends Component {
   constructor(props) {
@@ -16,7 +18,10 @@ class Applications extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return !["config", "applications"].includes(nextProps.modelEvent);
+    if (!shallowEqual(this.state, nextState)) {
+      return true;
+    }
+    return ["config", "applications"].includes(nextProps.modelEvent);
   }
 
   toggleModalAddApplication() {
@@ -26,12 +31,78 @@ class Applications extends Component {
   }
 
   render() {
+    const self = this;
+    const applicationsRows = [];
+    self.props.applications.d.forEach(function(application, idx) {
+      applicationsRows.push(
+        <ApplicationRow
+          config={self.props.config}
+          key={idx}
+          application={application}
+        />
+      );
+    });
+
     return (
       <div className="ezmaster-applications">
         <Helmet>
           <title>EzMaster - Applications</title>
         </Helmet>
-        TODO APPLICATIONS
+        {/* append a "add application" button if the table items are more than 10 items so that it's easier to click on the button */}
+        {applicationsRows.length > 10 && (
+          <Row>
+            <Col>
+              <Button
+                className="ml-2 ezmaster-applications-add ezmaster-applications-add-top"
+                color="primary"
+                onClick={this.toggleModalAddApplication}
+              >
+                <i className="fa fa-plus-circle" /> Add application
+              </Button>
+            </Col>
+          </Row>
+        )}
+
+        <Row>
+          <Col>
+            <Table
+              striped={true}
+              responsive={true}
+              className="ezmaster-applications-table"
+            >
+              <thead>
+                <tr>
+                  <th>Application name</th>
+                  <th>Creation date</th>
+                  <th>Size</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>{applicationsRows}</tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              className="ml-2 ezmaster-applications-add"
+              color="primary"
+              onClick={this.toggleModalAddApplication}
+            >
+              <i className="fa fa-plus-circle" /> Add application
+            </Button>
+
+            {/*
+            <ModalAddInstance
+              config={this.props.config}
+              modalIsOpen={this.state.modalAddInstanceIsOpen}
+              applications={this.props.applications}
+              instances={this.props.instances}
+              toggle={this.toggleModalAddInstance}
+            />
+*/}
+          </Col>
+        </Row>
       </div>
     );
   }
