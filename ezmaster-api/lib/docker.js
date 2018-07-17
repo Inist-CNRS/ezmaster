d7ceaa93fdfcea9fd72d8177f7c484cb1bd011ec
Stéphane Gully <stephane.gully@gmail.com>
2018-07-17 22:45:46 +0200

    prettier

--- ezmaster-api/lib/docker.js
+++ ezmaster-api/lib/docker.js
@@ -4,19 +4,18 @@
  * - docker-event object
  */
 
-'use strict';
+"use strict";
 
-var path       = require('path');
-var basename   = path.basename(__filename, '.js');
-var debug      = require('debug')('ezmaster:' + basename);
+var path = require("path");
+var basename = path.basename(__filename, ".js");
+var debug = require("debug")("ezmaster:" + basename);
 
-var DockerEvents = require('docker-events');
-var Docker       = require('dockerode');
+var DockerEvents = require("docker-events");
+var Docker = require("dockerode");
 
 var shared = {};
-shared.docker  = new Docker({ socketPath: '/var/run/docker.sock'});
+shared.docker = new Docker({ socketPath: "/var/run/docker.sock" });
 shared.emitter = new DockerEvents({ docker: shared.docker });
 shared.emitter.start();
-
 
 module.exports = shared;
--- ezmaster-api/routes/v1-config.js
+++ ezmaster-api/routes/v1-config.js
@@ -1,16 +1,16 @@
 /*eslint-env node */
 /*eslint no-sync: "off", global-require: "off"*/
-'use strict';
+"use strict";
 
-var cfg     = require('../lib/config.js');
-var _       = require('lodash');
-var express = require('express');
-var router  = express.Router();
+var cfg = require("../lib/config.js");
+var _ = require("lodash");
+var express = require("express");
+var router = express.Router();
 
 /**
  * Returns the ezmaster public configuration parameters
  */
-router.route('/').get(function (req, res, next) {
+router.route("/").get(function(req, res, next) {
   return res.status(200).send(_.pick(cfg, cfg.publicParameters));
 });
 
