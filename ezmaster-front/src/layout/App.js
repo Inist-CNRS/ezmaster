462cce8f48bcf3c4ea40d5c987baffb6a055e17a
Stéphane Gully <stephane.gully@gmail.com>
2018-07-17 17:48:13 +0200

    fix router problem when reverse proxy mode is enabled

--- ezmaster-front/src/layout/App.js
+++ ezmaster-front/src/layout/App.js
@@ -36,8 +36,8 @@ class App extends Component {
             modelEvent={this.props.modelEvent}
             config={this.props.config}
             infoMachine={this.props.infoMachine}
-            applicationsActive={window.location.pathname === "/applications/"}
-            instancesActive={window.location.pathname === "/instances/"}
+            applicationsActive={window.location.hash === "#/applications/"}
+            instancesActive={window.location.hash === "#/instances/"}
           />
         </div>
         <div
